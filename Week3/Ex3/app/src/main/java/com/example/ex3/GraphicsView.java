1deaf738d3a293cca2edfdd7bbff998a5472d9ea
kieuoanh2512 <91962499+kieuoanh2512@users.noreply.github.com>
2023-04-19 16:07:03 +0700

    upload

---
+++ Week3/Ex3/app/src/main/java/com/example/ex3/GraphicsView.java
@@ -0,0 +1,48 @@
+package com.example.ex3;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class GraphicsView extends View {
+    Bitmap[] frames = new Bitmap [16]; //16 frames
+    int i=0;
+    public GraphicsView(Context context){
+        super(context);
+
+        frames [0] = BitmapFactory.decodeResource (getResources () ,R.drawable.img1);
+        frames [1] = BitmapFactory.decodeResource (getResources () , R.drawable.img2);
+        frames [2] = BitmapFactory.decodeResource (getResources () , R.drawable.img3);
+        frames [3] = BitmapFactory. decodeResource (getResources () , R.drawable.img4);
+        frames [4] = BitmapFactory.decodeResource (getResources () , R.drawable.img5);
+        frames [4] = BitmapFactory.decodeResource (getResources () , R.drawable.img6);
+        frames [4] = BitmapFactory.decodeResource (getResources () , R.drawable.img8);
+        frames [5] = BitmapFactory.decodeResource (getResources () ,R.drawable.img1);
+        frames [6] = BitmapFactory.decodeResource (getResources () , R.drawable.img2);
+        frames [7] = BitmapFactory.decodeResource (getResources () , R.drawable.img3);
+        frames [8] = BitmapFactory. decodeResource (getResources () , R.drawable.img6);
+        frames [9] = BitmapFactory.decodeResource (getResources () , R.drawable.img4);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (i < 16){
+            canvas.drawBitmap(frames[i], 40, 40, new Paint());
+        }
+        else {
+            i =0;
+        }
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        i++;
+        return true;
+    }
+}
